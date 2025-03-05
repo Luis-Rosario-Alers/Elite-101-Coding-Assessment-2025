8f22ce57e1692c487d384d48b577452274f450d9
Luis Rosario Alers <luisrosarioalers@gmail.com>
2025-03-04 19:52:51 -0600

    :bug: fix logic errors

--- solutions.py
+++ solutions.py
@@ -19,8 +19,7 @@ def list_all_free_tables(time_slot: int, table_data: list) -> list:
     LEVEL 1: this function is used to return the free tables in a specific time slot.
     """
     free = []  # create our output list, this will output our list of free tables
-    for occupied_state in range(1,
-                                len(table_data)[time_slot]):  # here we check make a for loop that skips the first index
+    for occupied_state in range(1, len(table_data[time_slot])):  # here we check make a for loop that skips the first index
         # of the chosen time slot because we know that will just contain the number of the time slot and not an actual
         # useful piece of data.
         if table_data[time_slot][
@@ -30,14 +29,14 @@ def list_all_free_tables(time_slot: int, table_data: list) -> list:
     return free  # return list of free tables.
 
 
-# print(list_all_free_tables(1, restaurant_tables2))
+print(list_all_free_tables(1, restaurant_tables2))
 
-def list_one_free_table_under_party_size(time_slot: int, table_data: list, party_size: int) -> list:
+def list_one_free_table_under_party_size(time_slot: int, table_data: list, party_size: int):
     """
         LEVEL 2: this function is used to return one free table in a specific time slot and equal
         to or above the party size.
         """
-    for occupied_state in range(1, len(table_data)[time_slot]):  # loop over the occupied state in given time slot
+    for occupied_state in range(1, len(table_data[time_slot])):  # loop over the occupied state in given time slot
         if table_data[time_slot][occupied_state] == 'o':  # if the table is open
             if int(table_data[0][occupied_state][
                        3]) >= party_size:  # then we check if the table is bigger than or equal to party size, if it is, we return it.
@@ -46,7 +45,7 @@ def list_one_free_table_under_party_size(time_slot: int, table_data: list, party
                 continue
 
 
-# print(list_one_free_table_under_party_size(1, restaurant_tables2, 2))
+print(list_one_free_table_under_party_size(1, restaurant_tables2, 2))
 
 def list_all_free_tables_under_party_size(time_slot: int, table_data: list, party_size: int) -> list:
     """
@@ -54,7 +53,7 @@ def list_all_free_tables_under_party_size(time_slot: int, table_data: list, part
         to or above the party size.
         """
     free = []  # output list
-    for occupied_state in range(1, len(table_data)[time_slot]):  # loop over the occupied state in given time slot
+    for occupied_state in range(1, len(table_data[time_slot])):  # loop over the occupied state in given time slot
         if table_data[time_slot][occupied_state] == 'o':  # if the table is open
             if int(table_data[0][occupied_state][
                        3]) >= party_size:  # then we check if the table is bigger than or equal to party size, if it is, we return it.
@@ -64,7 +63,7 @@ def list_all_free_tables_under_party_size(time_slot: int, table_data: list, part
     return free  # output free tables
 
 
-# print(list_all_free_tables_under_party_size(4, restaurant_tables2, 2))
+print(list_all_free_tables_under_party_size(4, restaurant_tables2, 2))
 
 
 def list_all_free_tables_and_combined_tables(time_slot: int, table_data: list, party_size: int) -> list:
@@ -89,7 +88,7 @@ def list_all_free_tables_and_combined_tables(time_slot: int, table_data: list, p
 print(list_all_free_tables_and_combined_tables(1, restaurant_tables2, 8))
 
 
-def list_all_free_tables_and_combines_tables_friendly(time_slot: int, table_data: list, party_size: int) -> list:
+def list_all_free_tables_and_combines_tables_friendly(time_slot: int, table_data: list, party_size: int):
     """
     BONUS: this function is used to return the free tables and adjacent tables in a specific time slot, equal
     to or above the party size. Also, gives a user-friendly response.
@@ -112,17 +111,18 @@ def list_all_free_tables_and_combines_tables_friendly(time_slot: int, table_data
             next_table = table_data[0][occupied_state + 1]
             prev_is_open = table_data[time_slot][occupied_state - 1] == 'o'
             next_is_open = table_data[time_slot][occupied_state + 1] == 'o'
-            if prev_is_open and next_is_open and (get_table_capacity(prev_table) + get_table_capacity(next_table)) >= party_size: # if the capacity of both adjacent tables are bigger than or equal to the party size
+            if prev_is_open and next_is_open and (get_table_capacity(prev_table) + get_table_capacity(next_table)) >= party_size: # if the capacities of both adjacent tables are bigger
+                # than or equal to the party size,
                 # we append it to the free list inside a list to represent it is a combo of 2 tables.
                 free.append([prev_table, next_table])
 
     for tables in free: # looping through all the free tables
         if isinstance(tables, list): # if the element is a list, it means it is a combo of 2 tables
-            table1_size = tables[0][tables[0].find('(') + 1:tables[0].find(')')]
-            table2_size = tables[1][tables[1].find('(') + 1:tables[1].find(')')]
-            print(f"combining table {table1_size} + table {table2_size} will be enough to fit your party size.") # we then print to the user both tables combined will accommodate their party size
+            table1 = tables[0][tables[0].find('T') + 1:tables[0].find('(')]
+            table2 = tables[1][tables[1].find('T') + 1:tables[1].find('(')]
+            print(f"combining table {table1} + table {table2} will be enough to fit your party size.") # we then print to the user both tables combined will accommodate their party size
         else: # else we just print the table and say its free
-            print(f"table {tables[tables.find('(') + 1:tables.find(')')]} is free and big enough for your party.")
+            print(f"table {tables[tables.find('T') + 1:tables.find('(')]} is free and big enough for your party.")
 
 
 list_all_free_tables_and_combines_tables_friendly(1, restaurant_tables2, 8)
